87c63832c715e8a8a62c61b6eed2fa1c53ea611e
unknown <avishek.npt@gmail.com>
2025-09-10 17:38:31 +0530

    modified base route 4x

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/E-BOOK-PDF/",
+  base: "https://avigithubb.github.io/E-BOOK-PDF/",
 })
  
